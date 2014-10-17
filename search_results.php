34c5974d025941e2e3c5404204f3ec7285349827
mkontogiorgis <mkontogiorgis@yahoo.gr>
2014-10-18 00:21:23 +0300

    last update of my exercise

--- search_results.php
+++ search_results.php
@@ -3,30 +3,53 @@
 <body>
 
 <?php
-	$movies = array(
-					"comedy" => array("The Hangover","Knocked Up","Due Date"),
+	$movies = array("comedy" => array("The Hangover","Knocked Up","Due Date"),
 					"drama" => array("The Shawshank Redemption","The Green Mile","Requiem for a Dream"),
 					"horror" => array("Halloween","The Conjuring","The Shining")
-					);
-$categories = array("comedy", "drama", "horror");
-$selected_key = $_POST["category"];
-$selected_val = $categories[$_POST["category"]];
+				   );
 
-echo "Search Result";
+	$selected_key = $_POST["category"];
+	$string_to_find = $_POST["search_string"];
+	$search_result = "";
+	
+	echo "Search Result for: <b>", $string_to_find, "</b>  in  <b>", $selected_key, "</b>  category";
+	
+	if (array_key_exists($selected_key, $movies)) 
+	{
+		foreach($movies[$selected_key] as $movie)
+		{
+			$pos = strpos($movie, $string_to_find);	
+			if (!($pos === false))
+			{
+				$search_result = "<b>".$movie."</b>";
+				echo "<br>", $search_result;
+			}
+		}	
+	}
+	else
+	{
+		foreach($movies as $key => $val)
+		{
+			foreach($val as $movie)
+			{
+				$pos = strpos($movie, $string_to_find);	
+				if (!($pos === false))
+				{
+					$search_result = "<b>".$movie."</b> (".$key.")";
+					echo "<br>", $search_result;
+				}
+			}	
+		}
+	}
+	
+	if ($search_result == "")
+	{
+		echo "<br> not found!";
+	}
+		
+		
+	
 ?>
-<br>
-<?php
-	echo $_POST["search_string"]
-?>
-<br>
-<?php
-	echo $selected_key
-?>
-<br>
-<?php
-	echo $selected_val
-?>
-<br>
-<input type="text" name="result" value=<?php $selected_key ?>>
+
 </body>
 </html>
